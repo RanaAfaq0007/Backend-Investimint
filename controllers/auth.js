814bc9e90013cccc37cff2d1d7ee4da8a79e8b9a
RanaAfaq0007 <97694513+RanaAfaq0007@users.noreply.github.com>
2023-06-19 04:56:11 +0500

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -149,19 +149,6 @@ export const login = async (req, res, next) => {
       user.password
     );
     if (!isPasswordCorrect) return next(createError(400, "Wrong password or username!"));
-      if (!user.verified) {
-        const token = await Token.findOne({ userId: user._id });
-        if (!token) {
-          token = await new Token({
-            userId: user._id,
-            token: crypto.randomBytes(32).toString("hex"),
-          }).save();
-          const url = `${process.env.BASE_URL}/Emailverify/${newUser._id}/${token.token}`;
-          await sendEmail(user.email, "Verify Email", url);
-        }
-      }
-  
-
     const { password, ...otherDetails } = user._doc;
     res
       .cookie("access_token", token, {
